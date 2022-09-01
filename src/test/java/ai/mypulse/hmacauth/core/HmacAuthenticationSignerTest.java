c6b3fcbc0269622da61873e066bef0a7e81ca8ee
Francesco Losciale <francesco.losciale@mypulse.ai>
2022-09-01 11:36:59 +0100

    [#TELE-1408] Test HMAC signature with query parameters
    Authors: @callwait @francesco-losciale

--- src/test/java/ai/mypulse/hmacauth/core/HmacAuthenticationSignerTest.java
+++ src/test/java/ai/mypulse/hmacauth/core/HmacAuthenticationSignerTest.java
@@ -16,7 +16,7 @@ public class HmacAuthenticationSignerTest {
     @Test
     public void calculateSignatureWhenGetRequestCreatesSignature() throws IOException {
         var auth = new HmacAuthenticationSigner();
-        var signatureRequest = createSignatureRequest("GET", null);
+        var signatureRequest = createSignatureRequest("GET", null, null);
         var expectedResult = "947bd0fa6994f6ddf7ccbddeff6968a8f011187b88864f1f8ba4f3fb48a2d9f0";
 
         var result = auth.calculateSignature(signatureRequest);
@@ -32,7 +32,7 @@ public class HmacAuthenticationSignerTest {
             put("fieldB", "valueB");
         }};
         byte[] requestBody = new ObjectMapper().writeValueAsBytes(values);
-        var signatureRequest = createSignatureRequest("POST", requestBody);
+        var signatureRequest = createSignatureRequest("POST", requestBody, null);
 
         var expectedResult = "d67b4f69c9373d224f9415dce5cdbeba484435b40b523d2b2d6f192b0bef333a";
         var result = auth.calculateSignature(signatureRequest);
@@ -40,7 +40,19 @@ public class HmacAuthenticationSignerTest {
         assertEquals(expectedResult, result);
     }
 
-    private AuthSignerRequest createSignatureRequest(String method, byte[] requestBody){
+    @Test
+    public void calculateSignatureWhenGetWithQueryStringRequestCreatesSignature() throws IOException {
+        var auth = new HmacAuthenticationSigner();
+        var queryString = "paramC=valueC&paramB=valueB&paramA=valueA";
+        var signatureRequest = createSignatureRequest("GET", null, queryString);
+
+        var expectedResult = "10ed21705b857eb52ab73296367e06db3f547308f8760ea498641cc25c425b3e";
+        var result = auth.calculateSignature(signatureRequest);
+
+        assertEquals(expectedResult, result);
+    }
+
+    private AuthSignerRequest createSignatureRequest(String method, byte[] requestBody, String queryString){
         var signatureTimestamp = Instant.now(Clock.fixed(Instant.parse("2022-01-01T14:00:00Z"),
                 ZoneOffset.UTC)).getEpochSecond();
         var accessKey = "test-access-key";
@@ -51,6 +63,7 @@ public class HmacAuthenticationSignerTest {
         request.setPath("/foo");
         request.setTimestamp(signatureTimestamp);
         request.setAccessKeyId(accessKey);
+        request.setQueryString(queryString);
 
         if (requestBody != null){
             request.setBody(requestBody);
